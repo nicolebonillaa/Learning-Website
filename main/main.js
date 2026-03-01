39ddabec6b930a427c94bf2b96ecc442f1fe92ee
Nicole Uribe <nicoleuribe@Nicoles-MacBook-Pro-7.local>
2026-02-28 20:30:40 -0800

    Finished homepage layout

--- main/main.js
+++ main/main.js
@@ -1,1 +1,6 @@
 //Learning website focused on teaching computer science concepts through brief lessons.
+
+//change navbar styles on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
