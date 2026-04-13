a810bf528d857208d79cc6c7fc84a0327dc4b58b
Nicole Uribe <nicoleuribe@Nicoles-MacBook-Pro-310.local>
2026-04-12 21:06:26 -0700

    Start of settings page

--- main/main.js
+++ main/main.js
@@ -5,4 +5,36 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 })
 
-//lessons questions for each lesson
+// Settings tab bar toggle
+const settingsLinks = document.querySelectorAll('.settings__menu a');
+const settingsContents = document.querySelectorAll('.settings__content section');
+
+if (settingsLinks.length > 0) {
+    settingsContents.forEach((s, i) => {
+        s.style.display = i === 0 ? 'block' : 'none';
+    });
+
+    settingsLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            const target = e.target.getAttribute('href');
+            if (!target.startsWith('#')) return;
+            e.preventDefault();
+
+            settingsContents.forEach(s => {
+                s.style.display = s.id === target.slice(1) ? 'block' : 'none';
+            });
+
+            settingsLinks.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+}
+
+//Signout functionality
+const signOutBtn = document.getElementById('signoutbtn');
+if (signOutBtn) {
+  signOutBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '/pages/login.html';
+  });
+}
